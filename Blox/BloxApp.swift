4f94032a025b9eae4287c01ca70772bab99307e5
Laura Chambers <laura.bliss@gmail.com>
2021-07-01 11:17:01 -0700

    Initial Commit

---
+++ Blox/BloxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloxApp.swift
+//  Blox
+//
+//  Created by Laura Chambers on 2021-07-01.
+//
+
+import SwiftUI
+
+@main
+struct BloxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
